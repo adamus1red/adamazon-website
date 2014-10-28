63902e23b8cb9e23c0fd592b533b22062ab5f4d4
MrRandom <adamus1red@gmail.com>
2014-10-28 12:28:45 +0000

    add search

--- includes/heading.php
+++ includes/heading.php
@@ -1,11 +1,15 @@
 		<div class="heading">
-			<form class="pull-right" role="form">
+			<form class="search" method="POST" action="search.php">
+				<input type="text" placeholder="Search">
+				<button class="button search" type="submit">Search</button>
+			</form>
+			<form class="pull-right" role="form" method="POST" action="login.php">
             <div class="form-group">
               <input type="text" placeholder="Email" class="form-control">
             </div>
             <div class="form-group">
               <input type="password" placeholder="Password" class="form-control">
             </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
+            <button type="submit" class="button">Sign in</button>
           </form>
 		</div>
