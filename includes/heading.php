68d7194bc386c47e011e4088af36e022be08e121
MrRandom <adamus1red@gmail.com>
2014-10-28 12:24:35 +0000

    add button

--- includes/heading.php
+++ includes/heading.php
@@ -1,3 +1,11 @@
 		<div class="heading">
-			
+			<form class="pull-right" role="form">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
 		</div>
