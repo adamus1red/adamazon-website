29e6b22a8eed0d508780bdf626b389b6196da2fa
adamus1red <adamus1red@user.github.com>
2014-10-29 23:53:50 +0000

    make login details appear on the same line as the search box

--- includes/heading.php
+++ includes/heading.php
@@ -1,20 +1,16 @@
 		<div class="heading">
             <div class="heading-content">
-			<h1 class="pull-left" id="logo">Adamazon</h1>
-			<form class="search" method="POST" action="search.php">
-				<input type="text" placeholder="Search">
-				<button class="button search" type="submit">Search</button>
-			</form>
-			<form class="pull-right login" role="form" method="POST" action="login.php">
-            <div class="form-login">
-              <input type="text" placeholder="Email" class="form-control">
+                <h1 class="pull-left" id="logo">Adamazon</h1>
+                <form class="search" method="POST" action="search.php">
+                    <input type="text" class="search-box"placeholder="Search">
+                    <button class="button search" type="submit">Search</button>
+                </form>
+                <form class="pull-right login" role="form" method="POST" action="login.php">
+                    <input type="text" placeholder="Email" class="form-control">
+                    <input type="password" placeholder="Password" class="form-control">
+                    <button type="submit" class="button">Sign in</button>
+                </form>
             </div>
-            <div class="form-login">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="button">Sign in</button>
-          </form>
-          </div>
 		</div>
 		<div class="content">
 		
