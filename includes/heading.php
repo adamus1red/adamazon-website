0e3369750934af012560c4ca7c2e2d8221bbb3c7
adamus1red <adamus1red@user.github.com>
2014-10-30 00:25:43 +0000

    change sizes

--- includes/heading.php
+++ includes/heading.php
@@ -1,15 +1,19 @@
 		<div class="heading">
             <div class="heading-content">
                 <h1 class="pull-left" id="logo">Adamazon</h1>
-                <form class="search" method="POST" action="search.php">
-                    <input type="text" class="search-box"placeholder="Search">
-                    <button class="button" type="submit">Search</button>
-                </form>
-                <form class="pull-right login" role="form" method="POST" action="login.php">
-                    <input type="text" placeholder="Email" class="form-control">
-                    <input type="password" placeholder="Password" class="form-control">
-                    <button type="submit" class="button">Sign in</button>
-                </form>
+                <div class="search">
+                    <form method="POST" action="search.php">
+                        <input type="text" class="search-box"placeholder="Search">
+                        <button class="button" type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="pull-right login">
+                    <form role="form" method="POST" action="login.php">
+                        <input type="text" placeholder="Email" class="form-control">
+                        <input type="password" placeholder="Password" class="form-control">
+                        <button type="submit" class="button">Sign in</button>
+                    </form>
+                </div>
             </div>
 		</div>
 		<div class="content center-block">
