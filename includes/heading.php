765ab33e0a41ddb5a1fc0573ba501da3d47aeab3
adamus1red <adamus1red@user.github.com>
2014-10-30 16:12:05 +0000

    add search post name

--- includes/heading.php
+++ includes/heading.php
@@ -3,7 +3,7 @@
                 <h1 class="pull-left" id="logo">Adamazon</h1>
                 <div class="search">
                     <form method="POST" action="search.php">
-                        <input type="text" class="search-box" placeholder="Search">
+                        <input type="text" class="search-box" name="searchTerm" placeholder="Search">
                         <button class="button" id="search" type="submit">Search</button>
                     </form>
                 </div>
