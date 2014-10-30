2d812c56e03f4959b82b82fc86907d07d38375cd
adamus1red <adamus1red@user.github.com>
2014-10-30 00:04:55 +0000

    undo and test new stuff

--- includes/heading.php
+++ includes/heading.php
@@ -1,27 +1,15 @@
-        <div class="heading">
+		<div class="heading">
             <div class="heading-content">
-                <table>
-                <tbody>
-                    <tr>
-                        <td class="nav-box-1">
-                            <h1 class="pull-left" id="logo">Adamazon</h1>
-                        </td>
-                        <td class="nav-box-2">
-                            <form class="search" method="POST" action="search.php">
-                                <input type="text" class="search-box" placeholder="Search">
-                                <button class="button search" type="submit">Search</button>
-                            </form>
-                        </td>
-                        <td class="nav-box-3">
-                            <form class="pull-right login" role="form" method="POST" action="login.php">
-                                <input type="text" placeholder="Email" class="form-control">
-                                <input type="password" placeholder="Password" class="form-control">
-                                <button type="submit" class="button">Sign in</button>
-                            </form>
-                        </td>
-                    </tr>
-                </tbody>
-                </table>
+                <h1 class="pull-left" id="logo">Adamazon</h1>
+                <form class="search" method="POST" action="search.php">
+                    <input type="text" class="search-box"placeholder="Search">
+                    <button class="button search" type="submit">Search</button>
+                </form>
+                <form class="pull-right login" role="form" method="POST" action="login.php">
+                    <input type="text" placeholder="Email" class="form-control">
+                    <input type="password" placeholder="Password" class="form-control">
+                    <button type="submit" class="button">Sign in</button>
+                </form>
             </div>
 		</div>
 		<div class="content">
