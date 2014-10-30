df372d896787b6271ee596c33429a1034c1fe529
adamus1red <adamus1red@user.github.com>
2014-10-30 00:01:42 +0000

    try new header style

--- includes/heading.php
+++ includes/heading.php
@@ -1,15 +1,27 @@
-		<div class="heading">
+        <div class="heading">
             <div class="heading-content">
-                <h1 class="pull-left" id="logo">Adamazon</h1>
-                <form class="search" method="POST" action="search.php">
-                    <input type="text" class="search-box"placeholder="Search">
-                    <button class="button search" type="submit">Search</button>
-                </form>
-                <form class="pull-right login" role="form" method="POST" action="login.php">
-                    <input type="text" placeholder="Email" class="form-control">
-                    <input type="password" placeholder="Password" class="form-control">
-                    <button type="submit" class="button">Sign in</button>
-                </form>
+                <table>
+                <tbody>
+                    <tr>
+                        <td class="nav-box-1">
+                            <h1 class="pull-left" id="logo">Adamazon</h1>
+                        </td>
+                        <td class="nav-box-2">
+                            <form class="search" method="POST" action="search.php">
+                                <input type="text" class="search-box" placeholder="Search">
+                                <button class="button search" type="submit">Search</button>
+                            </form>
+                        </td>
+                        <td class="nav-box-3">
+                            <form class="pull-right login" role="form" method="POST" action="login.php">
+                                <input type="text" placeholder="Email" class="form-control">
+                                <input type="password" placeholder="Password" class="form-control">
+                                <button type="submit" class="button">Sign in</button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+                </table>
             </div>
 		</div>
 		<div class="content">
