8d913d19e7f116a29cdec55327656d22c239d567
adamus1red <adamus1red@user.github.com>
2014-10-31 01:38:39 +0000

    remove debugging code

--- search.php
+++ search.php
@@ -40,7 +40,6 @@
         ?>
             </tbody>
         </table>
-        <p><?php print_r($result);?><br><?php echo $search;?><br><?php echo $sql;?></p>
 <?php
     } else {
         echo "    <form method=\"POST\" action=\"search.php\">\n".
