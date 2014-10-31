3efcacbbb890b75cf4dd394d796fe8aab0963aa3
adamus1red <adamus1red@user.github.com>
2014-10-31 20:56:38 +0000

    update product page with new code to display the product

--- product.php
+++ product.php
@@ -12,12 +12,23 @@
     $result = $oMySQL->executeSQL($sql);
     if($result!=1||!empty($result)) {
 		for($i = 0; $i < (count($result)/$prodCol); $i++){
-			echo "<article>\n".
-           	  "    <img src=\"". $base_url . "". $result['productImage'] ."\" class=\"product-image\"alt=\"" . $result['name'] . "\">\n".
-              "    <h2 class=\"prod-header\">". $result['name'] ."</h2>\n".
-              "    <p>" . $result['description'] . "</p>".
-              "<a id=\"basket\" class=\"button\" href=\"".$base_url."/basket.php?add=".$request."\">Add to basket</a>".
-              "</article>";
+			echo "<article class=\"prod-display\">\n".
+                 "<table style=\"width:100%\">\n".
+                 "    <tbody>\n".
+                 "        <tr>\n".
+                 "            <td class=\"prod-image\"rowspan=\"2\">\n".
+                 "                <a href=\"". $base_url ."/product.php?pID=". $result[$i]['prodID'] ."\">\n".
+                 "                    <img src=\"". $base_url . "". $result[$i]['productImage'] ."\" class=\"product-image\"alt=\"" . $result[$i]['name'] . "\">\n".
+                 "                </a>\n".
+                 "            </td>\n".
+                 "            <td class=\"prod-header\"><h2>". $result[$i]['name'] ."</h2></td>\n".
+                 "        </tr>\n".
+                 "        <tr>\n".
+                 "            <td class=\"prod-content\"><p>" . $result[$i]['description'] . "</p></td>\n".
+                 "        </tr>\n".
+                 "    </tbody>\n".
+                 "</table>\n".
+                 "</article>";
    	}
 	} else {
 		echo "<h3>No product found</h3>";	
