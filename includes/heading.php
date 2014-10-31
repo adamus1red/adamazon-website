6875e9ad9fc8182cc35f1ab27ecc6afd68d7f358
adamus1red <adamus1red@user.github.com>
2014-10-31 00:57:13 +0000

    derp. close your tags id-10T

--- includes/heading.php
+++ includes/heading.php
@@ -1,6 +1,6 @@
 		<div class="heading">
             <div class="heading-content">
-                <h1 class="pull-left" id="logo"><a href=<?php echo "". $base_url . "/";?>Adamazon</h1>
+                <h1 class="pull-left" id="logo"><a href=<?php echo "". $base_url . "/";?>>Adamazon</a></h1>
                 <div class="search">
                     <form method="POST" action="search.php" style="width:40%;">
                         <input type="text" class="search-box" name="searchTerm" placeholder="Search">
