cb00e48cd1e9b796de9d65dba4aee8821811b93e
adamus1red <adamus1red@user.github.com>
2014-11-01 23:42:04 +0000

    add some content to the footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
             </table>
         </div>        
         <footer class="footer footer-grad">
-            
+            <span class="pull-right">&copyright; TUF Corp.</span>
         </footer>
     </body>
 </html>
