cc9450837b3877e2f3559573ff68206381da330c
MrRandom <adamus1red@gmail.com>
2014-11-03 16:09:00 +0000

    update for user account stuff

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <?php
 include_once("mysql.php");
+include_once("config.php");
 /*
    if (eregi("MSIE",getenv("HTTP_USER_AGENT")) ||
        eregi("Internet Explorer",getenv("HTTP_USER_AGENT"))) {
@@ -7,12 +8,13 @@ include_once("mysql.php");
 	exit;
    }
    */
+   
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php echo $site_name;?> - <?php echo $site_page_title;?></title>
-		<link rel="stylesheet" href="<?php echo $base_url;?>/css/main.css">
-		<script type="text/javascript" src="<?php echo $base_url;?>/js/we-love-ie.js"></script>
+		<title><?php echo $config['site_name'];?> - <?php echo $site_page_title;?></title>
+		<link rel="stylesheet" href="<?php echo $config['base_url'];?>/css/main.css">
+		<script type="text/javascript" src="<?php echo $config['base_url'];?>/js/we-love-ie.js"></script>
 	</head>
 	<body>
--- includes/heading.php
+++ includes/heading.php
@@ -1,10 +1,11 @@
+<?php include_once("user.php"); $user = new User();?>
 		<div class="heading">
             <div class="heading-content header-grad">
                 <table style="width=100%;">
                     </tbody>
                         <tr >
                             <td style="width=103px;">
-                                <a href=<?php echo "". $base_url . "/";?>><h1 class="pull-left" id="logo">Adamazon</h1></a>
+                                <a href=<?php echo "". $config['base_url'] . "/";?>><h1 class="pull-left" id="logo">Adamazon</h1></a>
                             </td>
                             <td class="search">
                                 <form method="POST" action="search.php" style="margin-right: auto; margin-left: auto; display: inline-block;">
@@ -13,11 +14,21 @@
                                 </form>
                             </td>
                             <td class="pull-right login">
-                                <form role="form" method="POST" action="login.php">
-                                    <input type="text" placeholder="Email" class="login-box">
-                                    <input type="password" placeholder="Password" class="login-box">
-                                    <button type="submit" id="login" class="button">Sign in</button>
-                                </form>
+                            	<?php 
+											$user = $_COOKIE["adamazon_user"];
+											$session = $_COOKIE["adamazon_session"];
+											if($user!="BLANK" && $session!="BLANK") {
+												if($user->checkSession()==0){
+													echo "<h3>" . $user->getUsername() . "</h3>"; 
+												}
+											} else {
+                                		echo "<form role=\"form\" method=\"POST\" action=\"login.php\">".
+                                    "<input type=\"text\" placeholder=\"Email\" class=\"login-box\">".
+                                    "<input type=\"password\" placeholder=\"Password\" class=\"login-box\">".
+                                    "<button type=\"submit\" id=\"login\" class=\"button\">Sign in</button>".
+                                "</form>";
+                             		}
+                              ?>
                             </td>
                         </tr>
                     </tbody>
@@ -32,14 +43,14 @@
                             <ul class="sidebar-content">
                             <?php 
                                 if(basename(__FILE__) != 'index.php'){
-                                    echo "<li><a href=\"". $base_url . "/\">Home</a></li>";
+                                    echo "<li><a href=\"". $config['base_url'] . "/\">Home</a></li>";
                                 }
                                 $oMySQL = new MySQL($mysql_database, $mysql_user, $mysql_pass, $mysql_host);
                                 $sql = "SELECT * FROM `category` WHERE `category`.`active` IS TRUE";
                                 $result = $oMySQL->executeSQL($sql);
                                 // Loop over results
                                 for($i = 0; $i < (count($result)/$catCol); $i++){
-                                    echo "<li><a href=\"" .$base_url ."/category.php?cat=" . $result['catID'] . "\">". $result['catName'] ."</a></li>";
+                                    echo "<li><a href=\"" .$config['base_url'] ."/category.php?cat=" . $result['catID'] . "\">". $result['catName'] ."</a></li>";
                                 }
                             ?>
                             </ul>
