371205a718dcb95850b53e60696af5111071bb97
adamus1red <adamus1red@user.github.com>
2014-11-03 02:06:12 +0000

    Fix wrong code for copyright

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
         </div>        
         <footer class="footer footer-grad">
             <p class="footer-content">
-                <span class="pull-right">&copyright; TUF Corp.</span>
+                <span class="pull-right">&copy; <abbr title="The Useless Fucks">TUF</abbr> Corp.<!-- sans Darren, 'cos he can't code for shit--></span>
             </p>
         </footer>
     </body>
