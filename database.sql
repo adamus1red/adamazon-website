9049251380ecf38fbccbbc5c5b58cd34e953e5c9
adamus1red <adamus1red@users.noreply.github.com>
2014-11-19 16:13:50 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: devweb2014.cis.strath.ac.uk:3306
--- Generation Time: Nov 03, 2014 at 02:17 AM
+-- Generation Time: Nov 19, 2014 at 04:13 PM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.4
 
@@ -39,6 +39,7 @@ CREATE TABLE IF NOT EXISTS `category` (
 --
 
 INSERT INTO `category` (`catID`, `catName`, `active`) VALUES
+(1, 'Offensive t-shirts', 1),
 (999, 'Test', 1);
 
 -- --------------------------------------------------------
@@ -61,6 +62,7 @@ CREATE TABLE IF NOT EXISTS `is_cat` (
 --
 
 INSERT INTO `is_cat` (`catID`, `prodID`) VALUES
+(1, 1),
 (999, 998),
 (999, 999);
 
@@ -86,8 +88,82 @@ CREATE TABLE IF NOT EXISTS `items` (
 --
 
 INSERT INTO `items` (`prodID`, `name`, `description`, `productImage`, `price`, `active`) VALUES
+(1, 'Offensive t-Shirt', 'This t-shirt will annoy your friends', '/images/product/001.png', 99.99, 1),
 (998, 'Test', 'This an item for testing', '/images/product/default.jpg', 99.55, 1),
 (999, 'Test Shirt', 'TEST', '/images/product/default.jpg', 50.99, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `orderID` int(11) NOT NULL AUTO_INCREMENT,
+  `prodID` int(11) NOT NULL,
+  `quantity` int(4) NOT NULL,
+  `final` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`orderID`,`prodID`),
+  KEY `prodID` (`prodID`),
+  KEY `orderID` (`orderID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`orderID`, `prodID`, `quantity`, `final`) VALUES
+(1, 1, 5, 0),
+(1, 999, 1, 0),
+(2, 1, 5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordered`
+--
+
+DROP TABLE IF EXISTS `ordered`;
+CREATE TABLE IF NOT EXISTS `ordered` (
+  `orderID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `total` int(10) NOT NULL,
+  PRIMARY KEY (`orderID`,`userID`),
+  UNIQUE KEY `orderID` (`orderID`),
+  KEY `userID` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ordered`
+--
+
+INSERT INTO `ordered` (`orderID`, `userID`, `total`) VALUES
+(1, 1, 500),
+(2, 1, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `sessionID` varchar(128) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1000 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `sessionID`, `active`) VALUES
+(1, 'Test', '7fb902bb439d254ee5a248678bc393a09f1dcfea', '456', 1);
 
 --
 -- Constraints for dumped tables
