dca9ba7d8daf2b31c1549ae682a828136a474342
darthtang <darthtang@hotmail.com>
2014-12-02 13:06:45 +0000

    om to the rescue

--- cookies.php
+++ cookies.php
@@ -1,2 +1,26 @@
 <?php
-	?>
+
+include_once("includes/config.php");
+include_once("includes/mysql.php");
+$site_page_title = "product";
+include("includes/header.php");
+include("includes/heading.php");
+$oMySQL = new MySQL($config['mysql_database'], $config['mysql_user'], $config['mysql_pass'], $config['mysql_host']);
+
+$cookie_userID = $_COOKIES['adamazon_login'];
+$cookie_orderID = $_COOKIES['adamazon_orderID'];
+
+
+if(isset($cookie_orderID)){
+	$sql = INSERT INTO `order` (`orderID`, `prodID`, `quantity`, `final`) VALUES;
+	$result = $oMySQL->executeSQL($sql);
+
+}
+
+if(!isset($cookie_orderID)){
+	$sql = INSERT INTO `order` (, `prodID`, `quantity`, `final`) VALUES;
+	$result = $oMySQL->executeSQL($sql);
+
+}
+setcookie(&cookie_userID,cookie_orderID);
+?>
