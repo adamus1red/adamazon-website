9b6be243ae30c2db59e78e2ab39f665da1fd102f
adamus1red <adamus1red@user.github.com>
2014-12-10 00:03:51 +0000

    comment the index page

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-    include_once("includes/config.php");
-    include_once("includes/mysql.php");
+    include_once("includes/config.php"); // Global config variables
+    include_once("includes/mysql.php"); // How to do mysql
    $site_page_title = "Home";
-   include("includes/header.php");
-    include("includes/heading.php");
-   $oMySQL = new MySQL($config['mysql_database'], $config['mysql_user'], $config['mysql_pass'], $config['mysql_host']);
-   $sql = "SELECT * FROM `items` WHERE `active` IS TRUE ORDER BY `items`.`prodID` DESC LIMIT 10";
-   $result = $oMySQL->executeSQL($sql);
-   if($result!=1){
-        for($i = 0; $i < count($result); $i++){
+   include("includes/header.php"); // Gives your <head>
+    include("includes/heading.php"); // Header with logo, search, userlogin and sidebar (should be moved)
+   $oMySQL = new MySQL($config['mysql_database'], $config['mysql_user'], $config['mysql_pass'], $config['mysql_host']); // Creates SQL connection
+   $sql = "SELECT * FROM `items` WHERE `active` IS TRUE ORDER BY `items`.`prodID` DESC LIMIT 10"; // Selectes the 10 newest items
+   $result = $oMySQL->executeSQL($sql); // executes the sql statement
+   if($result!=1){ // check we got a result
+        for($i = 0; $i < count($result); $i++){ // iterate through the results and output the data
             echo "<article class=\"prod-display\">\n".
                  "<table style=\"width:100%\">\n".
                  "    <tbody>\n".
@@ -28,6 +28,6 @@
                  "</article>";
         }
     } 
-    include("includes/footer.php");
+    include("includes/footer.php"); // footer (duh), also closes the table that formats everything iirc
 ?>
 
