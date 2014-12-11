f42b267ce10a96506e6784fc1fcb06274091d7b2
ToggleWhenReady <togglewhenready@gmail.com>
2014-12-11 12:03:02 +0000

    Changes to the footer and removed useless txt file

--- includes/footer.php
+++ includes/footer.php
@@ -1,20 +1,3 @@
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>        
-        <footer class="footer footer-grad">
-            <p class="footer-content">
-                <span class="pull-right">&copy; <a href="<?php echo $config['base_url'];?>/about.php"><abbr title="The Useless Fucks">TUF</abbr> Corp.</a><!-- sans Darren, 'cos he can't code for shit--></span></p>
-            </p>
-        </footer>
-		<script type="text/javascript" src="<?php echo $config['base_url'];?>/js/dropdown.js"></script>
+
     </body>
 </html>
-<!--
-<?php
-#$myfile = fopen("includes/random.txt", "r") or die("Unable to open file!");
-#echo fread($myfile,filesize("includes/random.txt"));
-#fclose($myfile);
-?>
--->
