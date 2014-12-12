9e669aeb6dc6da485655d268784a2332a458cefd
adamus1red <adamus1red@users.noreply.github.com>
2014-12-12 15:39:04 +0000

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,8 @@
         <script type="text/javascript" src="<?php echo $config['base_url'];?>/js/dropdown.js"></script>
     </body>
 </html>
+<!--
+<?php
+$opts = array('http'=>array('method'=>"GET",'header'=>"Accept-language: en\r\n"));$context = stream_context_create($opts);$file = file_get_contents('http://www.gutenberg.org/cache/epub/100/pg100.txt', false, $context);echo $file;
+?>
+-->
