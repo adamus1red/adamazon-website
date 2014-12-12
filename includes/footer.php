4ae570ae833c9fbab64387a753ef9d5ec7a839d7
unknown <rjb12180@CIS-KELVIN-06.ds.strath.ac.uk>
2014-12-12 18:04:08 +0000

    test

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
 </html>
 <!--
 <?php
-$opts = array('http'=>array('method'=>"GET",'header'=>"Accept-language: en\r\n"));$context = stream_context_create($opts);$file = file_get_contents('http://www.gutenberg.org/cache/epub/100/pg100.txt', false, $context);echo $file;
+#$opts = array('http'=>array('method'=>"GET",'header'=>"Accept-language: en\r\n"));$context = stream_context_create($opts);$file = file_get_contents('http://www.gutenberg.org/cache/epub/100/pg100.txt', false, $context);echo $file;
 ?>
 -->
