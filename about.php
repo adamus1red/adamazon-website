dbe31d7711ba84ef5518374d6603a72da7d07331
unknown <rjb12180@CIS-MUIR-26.ds.strath.ac.uk>
2014-12-12 14:44:39 +0000

    quick mod to about page

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies elit in tortor semper, eget cursus justo dictum. Phasellus feugiat eros eget risus ullamcorper tempor. Nullam vitae risus eu est ultricies sodales sed id felis. Nunc ultrices elit id quam fringilla placerat. Nunc urna arcu, mattis vitae tortor eu, lobortis suscipit ante. Nulla mollis pulvinar finibus. Morbi sed justo ut diam consectetur ornare id ac enim. Vivamus ultricies vehicula justo vel gravida. Curabitur molestie odio hendrerit lectus accumsan ultrices. Morbi sagittis augue vitae imperdiet aliquam. Etiam aliquam dolor ligula, vel elementum sapien auctor ac. Sed at turpis vitae sem sodales rutrum sit amet ac quam. Suspendisse imperdiet ex enim, nec hendrerit tellus dictum et. Pellentesque mollis purus sit amet magna vehicula, at tincidunt magna ullamcorper. Integer quis consectetur justo, quis egestas turpis.</p>
             <hr>
             <h1 style="font-family: 'Crimson Text', serif;"><i>The Team</i></h1>
-            <table>
+            <table style="width: 100%;">
                 <tbody>
                     <tr style="padding: 10px;">
                         <td width="25%">
