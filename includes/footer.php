d847edb57af6ffa8243beb1a6059faaa580c60cd
ToggleWhenReady <togglewhenready@gmail.com>
2014-12-15 11:23:25 +0000

    Fix to footer
    Removing UNNEEDED Shakespeare

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,3 @@
 		<script type="text/javascript" src="<?php echo $config['base_url'];?>/js/dropdown.js"></script>
     </body>
 </html>
-<!--
-<?php
-$opts = array('http'=>array('method'=>"GET",'header'=>"Accept-language: en\r\n"));$context = stream_context_create($opts);$file = file_get_contents('http://proton.digitalwizardry.org/~adam/pg100.txt', false, $context);echo $file;
-?>
--->
